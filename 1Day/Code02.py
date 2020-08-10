ff2225da3c0217c9f0e70d43b9f8b4e20b59a31e
hgh <ymca0147@naver.com>
2020-08-10 20:50:00 +0900

    First Commit

---
+++ 1Day/Code02.py
@@ -0,0 +1,29 @@
+print("%d" % 123)
+print("%5d" % 123)
+print("%05d" % 123)
+
+print("")
+
+print("%f" % 123.45)
+print("%7.1f" % 12345678.45678)
+print("%7.3f" % 12345678.45678)
+
+print("")
+
+print("%s" % "Python")
+print("%10s" % "Python")
+
+print("")
+
+print("%d %5d %05d" % (123, 123, 123))
+print("{2:d} {1:d} {0:d}".format(100, 200, 300))
+
+print("한 행입니다. \n또 한행입니다")
+
+print("\n줄바꿈\n연습")
+print("\t탭키\t연습")
+print("글자가 \"강조\" 되는 효과1")
+print("글자가 \'강조\' 되는 효과2")
+print("\\\\\\ 역슬래시 3개 출력")
+print(r"\n \t \" \\를 그대로 출력")
+print("문제" + "없음")
